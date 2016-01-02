7423b827dfb5e53bc306fac864cf0a0cef35db0d
aleju <kontakt@ajung.name>
2016-01-02 22:55:24 +0100

    Fix various small bugs, simplify saving loop

--- apply_locator.py
+++ apply_locator.py
@@ -11,15 +11,17 @@ Usage:
     python apply_locator.py
 """
 from __future__ import division, print_function
+from dataset import Dataset
 import os
 import numpy as np
+import argparse
 import random
 import re
 from scipy import misc
 from scipy import ndimage
-from train import MODEL_IMAGE_HEIGHT, MODEL_IMAGE_WIDTH, BATCH_SIZE,
-                  SAVE_WEIGHTS_FILEPATH, create_model, create_model_tiny,
-                  draw_rectangle
+from train import MODEL_IMAGE_HEIGHT, MODEL_IMAGE_WIDTH, BATCH_SIZE, \
+                  SAVE_WEIGHTS_FILEPATH, create_model, create_model_tiny, \
+                  draw_predicted_rectangle
 from keras.optimizers import Adam
 from ImageAugmenter import ImageAugmenter
 
@@ -51,11 +53,11 @@ def main():
 
     # load images
     dataset = Dataset([args.dataset])
-    filenames = [os.basename(fp) for fp in dataset.fps] # will be used during saving
+    filenames = [os.path.basename(fp) for fp in dataset.fps] # will be used during saving
     nb_images = len(dataset.fps)
     X = np.zeros((nb_images, MODEL_IMAGE_HEIGHT, MODEL_IMAGE_WIDTH, 3), dtype=np.float32)
     paths = []
-    for i, fp, image in enumerate(zip(dataset.fps, dataset.get_images())):
+    for i, (fp, image) in enumerate(zip(dataset.fps, dataset.get_images())):
         #image.square()
         image.resize(MODEL_IMAGE_HEIGHT, MODEL_IMAGE_WIDTH)
         X[i] = image.image_arr / 255.0
@@ -63,34 +65,38 @@ def main():
     X = np.rollaxis(X, 3, 1)
 
     # create model
-    #model = create_model_tiny(MODEL_IMAGE_HEIGHT, MODEL_IMAGE_WIDTH, Adam())
-    model = create_model(MODEL_IMAGE_HEIGHT, MODEL_IMAGE_WIDTH, "mse", Adam())
+    model = create_model_tiny(MODEL_IMAGE_HEIGHT, MODEL_IMAGE_WIDTH, "mse", Adam())
     model.load_weights(args.weights)
 
     # predict positions of faces
     preds = model.predict(X, batch_size=BATCH_SIZE)
 
     # Calculate exact coordinates of faces on original images
+    """
     coords = []
     for idx, (y, x, half_height, half_width) in enumerate(preds):
-        orig_height = X[idx].shape[2]
-        orig_width = X[idx].shape[3]
+        orig_height = X[idx].shape[1]
+        orig_width = X[idx].shape[2]
         tl_y, tl_x, br_y, br_x = unnormalize_prediction(y, x, half_height, half_width, img_height=orig_height, img_width=orig_width)
-        """keypoints = np.zeros((9*2,), dtype=np.uint16)
-        keypoints[0:4] = [tl_y, tl_x, br_y, br_x]
-        image = ImageWithKeypoints(X[idx], Keypoints(keypoints))
-        image.resize()"""
 
         coords.append((tl_y, tl_x, br_y, br_x))
 
     # Save images
     print("Saving images...")
     for idx, (tl_y, tl_x, br_y, br_x) in enumerate(coords):
+        print(X.shape, X[idx].shape)
         img = draw_rectangle(X[idx], tl_y, tl_x, br_y, br_x)
         filepath = os.path.join(WRITE_TO_DIR, filenames[idx])
         misc.imsave(filepath, img)
+    """
 
-def get_images(dirs):
+    print("Saving images...")
+    for idx, (y, x, half_height, half_width) in enumerate(preds):
+        img = draw_predicted_rectangle(X[idx], y, x, half_height, half_width)
+        filepath = os.path.join(WRITE_TO_DIR, filenames[idx])
+        misc.imsave(filepath, img)
+
+def get_images2(dirs):
     """Collects all images in given directories.
     Args:
         dirs: List of directories.
@@ -106,7 +112,7 @@ def get_images(dirs):
         result.append(image)
     return result, paths
 
-def get_image_paths(dirs):
+def get_image_paths2(dirs):
     """Collects filepaths of all images in given directories.
     Args:
         dirs: List of directories.
