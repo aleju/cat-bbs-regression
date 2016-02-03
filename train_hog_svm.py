e696ad796b149cff410fd404b2380ad7d228124f
aleju <kontakt@ajung.name>
2016-02-03 23:06:52 +0100

    Refactor loading functions

--- train_hog_svm.py
+++ train_hog_svm.py
@@ -68,7 +68,7 @@ def main():
     acc = svc.score(X_val, y_val)
     print("accuracy = %.4f" % (acc))
 
-def load_xy(dataset, nb_load, nb_augmentations):
+def load_xy(dataset, nb_crops_max, nb_augmentations):
     """Loads X and y (examples with labels) for the dataset.
     Examples are HOGs of 32x32 crops of 256x256 images.
     Labels are 0/1 whether the crop contains a cat.
@@ -82,53 +82,79 @@ def load_xy(dataset, nb_load, nb_augmentations):
         y (numpy array of shape (N, 1))
     """
     nb_crops_per_image = (MODEL_IMAGE_HEIGHT // CROP_HEIGHT) * (MODEL_IMAGE_WIDTH // CROP_WIDTH)
-    nb_load = min(nb_load, len(dataset.fps) * nb_crops_per_image)
-    nb_crops = nb_load + nb_load * nb_augmentations
-    #X = np.zeros((nb_images, CROP_HEIGHT, CROP_WIDTH), dtype=np.float32)
-    X = np.zeros((nb_crops, 8100), dtype=np.float32)
-    y = np.zeros((nb_crops,), dtype=np.float32)
+    nb_crops_possible = len(dataset.fps) * nb_crops_per_image
+    nb_crops_max = min(nb_crops_max, nb_crops_possible)
+
+    X = np.zeros((nb_crops_max, 8100), dtype=np.float32)
+    y = np.zeros((nb_crops_max,), dtype=np.float32)
 
     #print("nb_crops_per_image=", nb_crops_per_image, "nb_load=", nb_load, "nb_crops=", nb_crops)
     nb_crops_added = 0
+    for i, (crop, face_factor) in enumerate(get_examples_with_labels(dataset, nb_crops_max, nb_augmentations)):
+        if nb_crops_added % 100 == 0:
+            print("Crop %d of %d" % (nb_crops_added+1, nb_crops_max))
+        crop_hog = hog(crop, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), normalise=True)
+        #crop_hog, vis = hog(crop, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), normalise=True, visualise=True)
+        #from scipy import misc
+        #misc.imshow(crop)
+        #misc.imshow(vis)
+        crop_hog[crop_hog < 0] = 0 # the hog values can rarely end up slightly below 0
+        is_cat = True if face_factor >= CAT_FRACTION_THRESHOLD else False
+        #if is_cat or random.random() < 0.25:
+        X[nb_crops_added] = crop_hog
+        y[nb_crops_added] = 1 if is_cat else 0
+        nb_crops_added += 1
+
+    return X, y
+
+def get_examples_with_labels(dataset, nb_crops_max, nb_augmentations,
+                             model_image_height=MODEL_IMAGE_HEIGHT,
+                             model_image_width=MODEL_IMAGE_WIDTH,
+                             crop_height=CROP_HEIGHT,
+                             crop_width=CROP_WIDTH,
+                             padding=PADDING,
+                             drop_nonface_prob=0.0):
+    """Loads X and y (examples with labels) for the dataset.
+    Examples are HOGs of 32x32 crops of 256x256 images.
+    Labels are 0/1 whether the crop contains a cat.
+
+    Args:
+        dataset            The Dataset object.
+        nb_load            Intended number of images to load.
+        nb_augmentations   Number of augmentations to perform.
+    Returns:
+        X (numpy array of shape (N, 32*32=1024)),
+        y (numpy array of shape (N, 1))
+    """
+    nb_crops_loaded = 0
     for img_idx, image in enumerate(dataset.get_images()):
-        image.resize(MODEL_IMAGE_HEIGHT, MODEL_IMAGE_WIDTH)
-        image.pad(PADDING)
+        image.resize(model_image_height, model_image_width)
+        image.pad(padding)
         augs = image.augment(nb_augmentations, hflip=True, vflip=False,
                              scale_to_percent=(0.9, 1.1), scale_axis_equally=False,
                              rotation_deg=10, shear_deg=0, translation_x_px=5, translation_y_px=5,
                              brightness_change=0.1, noise_mean=0.0, noise_std=0.05)
         for aug in [image] + augs:
-            aug.unpad(PADDING)
-            for crop, face_factor in create_crops(aug):
-                if nb_crops_added % 100 == 0:
-                    print("Adding crop %d of %d..." % (nb_crops_added+1, nb_crops))
+            aug.unpad(padding)
+            for crop, face_factor in image_to_crops(aug, crop_height, crop_width):
+                if drop_nonface_prob == 0.0 or random.random() > drop_nonface_prob:
+                    yield crop, face_factor
+                    nb_crops_loaded += 1
 
-                #print(crop.shape)
-                crop_hog = hog(crop, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), normalise=True)
-                #crop_hog, vis = hog(crop, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), normalise=True, visualise=True)
-                #from scipy import misc
-                #misc.imshow(crop)
-                #misc.imshow(vis)
-                crop_hog[crop_hog < 0] = 0 # the hog values can rarely end up slightly below 0
-                is_cat = True if face_factor >= CAT_FRACTION_THRESHOLD else False
-                if is_cat or random.random() < 0.25:
-                    X[nb_crops_added] = crop_hog
-                    y[nb_crops_added] = 1 if is_cat else 0
-                    nb_crops_added += 1
-
-                if nb_crops_added >= nb_crops:
+                if nb_crops_loaded >= nb_crops_max:
                     break
-            if nb_crops_added >= nb_crops:
+            if nb_crops_loaded >= nb_crops_max:
                 break
-        if nb_crops_added >= nb_crops:
+        if nb_crops_loaded >= nb_crops_max:
             break
 
-    return X, y
 
-def create_crops(img):
-    """Extracts all 32x32 crops from a given image.
+def image_to_crops(img, crop_height, crop_width):
+    """Extracts all NxM grayscale crops (patches) from a given image.
     Args:
-        img     The image to crop
+        img         The image to crop
+        crop_height Height of the crop
+        crop_width  Width of the crop
     Returns:
         Generator of (crop as numpy array, cat fraction as float 0.0 to 1.0)
     """
@@ -141,33 +167,26 @@ def create_crops(img):
 
     height = img_arr.shape[0]
     width = img_arr.shape[1]
-    nb_crops_y = height // CROP_HEIGHT
-    nb_crops_x = width // CROP_WIDTH
+    nb_crops_y = height // crop_height
+    nb_crops_x = width // crop_width
     nb_crops = nb_crops_y * nb_crops_x
 
     crop_tl_y = 0
     for grid_y in range(nb_crops_y):
         crop_tl_x = 0
         for grid_x in range(nb_crops_x):
-            #grid_y = i // nb_crops_x
-            #grid_x = i % nb_crops_x
-
-            #crop_tl_y = height * (CROP_HEIGHT * grid_y)
-            #crop_br_y = height * (CROP_HEIGHT * (grid_y + 1))
-            #crop_tl_x = width * (CROP_WIDTH * grid_x)
-            #crop_br_x = width * (CROP_WIDTH * (grid_y + 1))
-            crop_br_y = crop_tl_y + CROP_HEIGHT
-            crop_br_x = crop_tl_x + CROP_WIDTH
+            crop_br_y = crop_tl_y + crop_height
+            crop_br_x = crop_tl_x + crop_width
 
             img_arr_crop = img_arr[crop_tl_y:crop_br_y, crop_tl_x:crop_br_x]
             img_face_crop = img_face[crop_tl_y:crop_br_y, crop_tl_x:crop_br_x]
             face_px = np.count_nonzero(img_face_crop)
-            face_factor = face_px / (CROP_HEIGHT * CROP_WIDTH)
+            face_factor = face_px / (crop_height * crop_width)
 
             yield img_arr_crop, face_factor
 
-            crop_tl_x += CROP_WIDTH
-        crop_tl_y += CROP_HEIGHT
+            crop_tl_x += crop_width
+        crop_tl_y += crop_height
 
 if __name__ == "__main__":
     main()
