40a6b1e05da456428f969a0c072dcc57bac94116
aleju <kontakt@ajung.name>
2016-02-05 23:12:53 +0100

    Fix various problems/bugs

--- train_texture_svm.py
+++ train_texture_svm.py
@@ -12,20 +12,21 @@ import numpy as np
 import argparse
 import random
 import os
-from skimage import color
-from skimage.feature import hog
+from skimage.feature import greycomatrix, greycoprops
 from sklearn.svm import SVC
+from train_hog_svm import get_crops_with_labels
 
 np.random.seed(42)
 random.seed(42)
 
-SPLIT = 0.1
 MODEL_IMAGE_HEIGHT = 512
 MODEL_IMAGE_WIDTH = 512
 CROP_HEIGHT = 32
 CROP_WIDTH = 32
-NB_LOAD_IMAGES = 60000
-CAT_FRACTION_THRESHOLD = 0.4
+NB_CROPS = 7500
+NB_VALIDATION = 512
+NB_AUGMENTATIONS = 0
+CAT_FRACTION_THRESHOLD = 0.5
 
 def main():
     """Load images, train classifier, score classifier."""
@@ -42,13 +43,15 @@ def main():
     print("Loading images...")
     X, y = load_xy(dataset, NB_CROPS, NB_AUGMENTATIONS)
     assert X.dtype == np.float32
-    assert np.max(X) <= 1.0
-    assert np.min(X) >= 0.0
+    print("X min:", np.min(X))
+    print("X max:", np.max(X))
 
     # split train and val
     X_val, X_train = X[0:NB_VALIDATION, ...], X[NB_VALIDATION:, ...]
     y_val, y_train = y[0:NB_VALIDATION, ...], y[NB_VALIDATION:, ...]
     print("%d of %d values in y_train are 1, %d of %d values in y_val" % (np.count_nonzero(y_train), y_train.shape[0], np.count_nonzero(y_val), y_val.shape[0]))
+    print(X_train.shape, X_val.shape)
+    print(y_train.shape, y_val.shape)
 
     print("Training...")
     svc = SVC(C=0.001)
@@ -67,14 +70,14 @@ def load_xy(dataset, nb_crops_max, nb_augmentations):
     X = []
     y = []
 
-    examples = get_examples_with_labels(dataset, nb_crops_max, nb_augmentations,
-                                        model_image_height=MODEL_IMAGE_HEIGHT,
-                                        model_image_width=MODEL_IMAGE_WIDTH,
-                                        crop_height=CROP_HEIGHT, crop_width=CROP_WIDTH)
+    examples = get_crops_with_labels(dataset, nb_crops_max, nb_augmentations,
+                                     model_image_height=MODEL_IMAGE_HEIGHT,
+                                     model_image_width=MODEL_IMAGE_WIDTH,
+                                     crop_height=CROP_HEIGHT, crop_width=CROP_WIDTH)
 
     for i, (crop, face_factor) in enumerate(examples):
-        if nb_crops_added % 100 == 0:
-            print("Crop %d of %d" % (nb_crops_added+1, nb_crops_max))
+        if i % 100 == 0:
+            print("Crop %d of %d" % (i+1, nb_crops_max))
 
         distances = [1, 3, 5, 7]
         angles = [0*np.pi/3, 1*np.pi/3, 2*np.pi/3, 3*np.pi/3]
@@ -85,18 +88,18 @@ def load_xy(dataset, nb_crops_max, nb_augmentations):
 
         nb_matrices = len(distances) * len(angles)
         all_values = np.zeros((3*nb_matrices))
-        all_values[0:nb_matrices] = dissimilarities
-        all_values[nb_matrices:nb_matrices*2] = energies
-        all_values[nb_matrices*2:nb_matrices*3] = correlations
+        all_values[0:nb_matrices] = dissimilarities.flatten()
+        all_values[nb_matrices:nb_matrices*2] = energies.flatten()
+        all_values[nb_matrices*2:nb_matrices*3] = correlations.flatten()
 
         is_cat = True if face_factor >= CAT_FRACTION_THRESHOLD else False
         #if is_cat or random.random() < 0.25:
         X.append(all_values)
-        y.append(np.array([1]) if is_cat else np.array([0]))
+        y.append(1 if is_cat else 0)
 
     # all entries in X/Y same length
     assert len(set([len(row) for row in X])) == 1
-    assert len(set([len(row) for row in Y])) == 1
+    #assert len(set([len(row) for row in y])) == 1
 
     X = np.array(X, dtype=np.float32)
     y = np.array(y, dtype=np.float32)
